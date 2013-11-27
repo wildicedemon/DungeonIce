138acadb85961293c951f152fb7b83fc038977e2
wildicedemon <wildicedemon@hotmail.com>
2013-11-27 03:23:40 -0500

    Start of function constuction. Mostly AoE and Healing Functionality. Starting on some specialized spell casts.

--- Helpers.lua
+++ Helpers.lua
@@ -66,16 +66,16 @@ local el = EntityList("alive,attackable,onmesh,distanceto="..e.id..",maxdistance
 	PosSumY = (PosSumY / count)
 	PosSumZ = (PosSumZ / count)	
 end
-function AoEPlayerBasedHealthCheck(MaxSpellRadius,HPCheckAmount,CountChk)
-local MaxSpellRadius = 0  --//The Maximum Radius of the Spell from the Player.
-local HPCheckAmount = 0 --//The Health Threshhold for counting as a Target.
-local CountChk= 0 --//The Number Of Unit's whom meet the Spells Critira.
+function AoEPlayerBasedHealthCheck(MaxSpellRadius,HPCheckAmount,CountCheck)
+local MSRadius = MaxSpellRadius  --//The Maximum Radius of the Spell from the Player.
+local HPChkAmount = HPCheckAmount --//The Health Threshhold for counting as a Target.
+local CountChk= CountCheck --//The Number Of Unit's whom meet the Spells Critira.
 local Count = 0
-local el = EntityList("alive,myparty,maxdistance="..tostring(MaxSpellRadius))
+local el = EntityList("alive,myparty,maxdistance="..tostring(MSRadius))
 	if ( el ) then
 		local i,e = next(el)
 		while (i~=nil and e~=nil) do
-			if e.hp.percent < HPCheckAmount then
+			if e.hp.percent < HPChkAmount then
 				Count= Count + 1
 			end
 
@@ -89,23 +89,23 @@ local el = EntityList("alive,myparty,maxdistance="..tostring(MaxSpellRadius))
 		end  
 	end
 end
-function AoETargetBasedHPCheck(MaxSpellRange,MaxSpellRadius,HPCheckAmount,CountChk)
-local MaxSpellRange = 0 --//The Maximum Range of the Spell.
-local MaxSpellRadius = 0 --//The Maximum Radius of the Spell from the Player.
-local HPCheckAmount = 0 --//The Health Threshhold for counting as a Target.
-local CountChk= 0 --//The Number Of Unit's whom meet the Spells Critira.
+function AoETargetBasedHPCheck(MaxSpellRange,MaxSpellRadius,HPCheckAmount,CountCheck)
+local MSRange = MaxSpellRange --//The Maximum Range of the Spell.
+local MSRadius = MaxSpellRadius --//The Maximum Radius of the Spell from the Player.
+local HPChkAmount = HPCheckAmount --//The Health Threshhold for counting as a Target.
+local CountChk= CountCheck --//The Number Of Unit's whom meet the Spells Critira.
 local PrevCount = 0 --//The Previous Count Of Unit's whom meet the Spells Critira.
 local PrevID = 0 
 local EntityCount = 0
-local el = EntityList("alive,myparty,maxdistance="..tostring(MaxSpellRange))
+local el = EntityList("alive,myparty,maxdistance="..tostring(MSRange))
 	if ( el ) then
 	local i,e = next(el)
 	while (i~=nil and e~=nil) do
-	local el2 = EntityList("alive,myparty,distanceto="..e.id...",maxdistance="..tostring(MaxSpellRadius))
+	local el2 = EntityList("alive,myparty,distanceto="..e.id...",maxdistance="..tostring(MSRadius))
 		if ( el2 ) then
 			local i2,e2 = next(el2)
 			while (i2~=nil and e2~=nil) do
-					if e2.hp.percent <= HPCheckAmount then
+					if e2.hp.percent <= HPChkAmount then
 						EntityCount= EntityCount + 1
 					end
 				local i2,e2 = next(el2,i2)  
@@ -204,4 +204,129 @@ function GetTargetingParty()
 			i,entity = next(petlist,i)         
 		end    
 	end 
+end
+
+function DoesPlayerHaveBuff(BuffsToCheck)
+	local BuffsToChk = BuffsToCheck --//Id's of Buff's to check against current Player Buffs.
+	local PlayerBuffs = Player.buffs        
+	if (TableSize(PlayerBuffs) > 0) then 							
+		for buffid in StringSplit(BuffsToChk,",") do
+			if (tonumber(buffid) ~= nil) then
+				for i, buff in pairs(PlayerBuffs) do
+					if (buff.id == tonumber(buffid)) then--and buff.ownerid == PID) then
+						return true
+					end
+				end	
+			end
+		end
+	end
+	return false	      
+end
+function DoesPartyMemberHaveBuff(BuffsToCheck)
+	local Entity = 0
+	local BuffsToChk = BuffsToCheck --//Id's of Buff's to check against current Entity Buffs.
+	local AllyBuffs = 0
+	local partymemberlist = EntityList.myparty 
+	local PartyID = ""
+	if ( partymemberlist) then 
+		local i,entity = next(partymemberlist) 
+		while (i~=nil and entity ~=nil) 
+		AllyBuffs = entity.buffs
+			if (TableSize(AllyBuffs) > 0) then 							
+				for buffid in StringSplit(BuffsToChk,",") do
+					if (tonumber(buffid) ~= nil) then
+						for i, buff in pairs(AllyBuffs) do
+							if (buff.id == tonumber(buffid)) then--and buff.ownerid == PID) then
+								return true
+							end
+						end	
+					end
+				end	    
+			end		
+			i,entity = next(partymemberlist,i) 		
+		end	
+		return false
+	end
+--Returns the First Party member that does not have the buff/s checked.
+--Returns False if no member is found or not missing buff checked.
+function ReturnPartyMemberNotHaveBuff(BuffsToCheck)
+	local Entity = 0
+	local BuffsToChk = BuffsToCheck --//Id's of Buff's to check against current Entity Buffs.
+	local AllyBuffs = 0
+	local partymemberlist = EntityList.myparty 
+	local PartyID = ""
+	if ( partymemberlist) then 
+		local i,entity = next(partymemberlist) 
+		while (i~=nil and entity ~=nil) 
+		AllyBuffs = entity.buffs
+			if (TableSize(AllyBuffs) > 0) then 							
+				for buffid in StringSplit(BuffsToChk,",") do
+					if (tonumber(buffid) ~= nil) then
+						for i, buff in pairs(AllyBuffs) do
+							if not (buff.id == tonumber(buffid)) then--and buff.ownerid == PID) then
+								return entity
+							end
+						end	
+					end
+				end	    
+			end		
+			i,entity = next(partymemberlist,i) 		
+		end	
+		return false
+	end
+
+function DoesEntityHaveBuff(BuffsToCheck,Entity)
+	local Entity = 0
+	local BuffsToChk = BuffsToCheck --//Id's of Buff's to check against current Entity Buffs.
+	local EntityBuffs = Entity.buffs
+	if (TableSize(EntityBuffs) > 0) then 							
+		for buffid in StringSplit(BuffsToChk,",") do
+			if (tonumber(buffid) ~= nil) then
+				for i, buff in pairs(EntityBuffs) do
+					if (buff.id == tonumber(buffid)) then--and buff.ownerid == PID) then
+						return true
+					end
+				end	
+			end
+		end	    
+	end
+	return false
+end
+
+
+function PetPartyHealAssist()
+local pet = Player.pet
+local PetHeals = ActionList("type=11")
+local PetHeal = PetHeals[32]
+if pet.contentid == 1399 then
+	local PetHeal = PetHeals[36]
+end
+local acPetHeal = ActionList:Get(PetHeal.id,PetHealTarget.id,13)
+local PetHealTarget = GetBestSummonHealTarget()
+	if Debugy == "1" then
+		d("Entering Pet Heal Assist =  "..tostring(PetHealTarget.name))
+	end
+		if (PetHealTarget ~= nil) and(PetHealTarget.hp.percent < 100) then
+			if Debugy == "1" then
+				d("Preparing to Cast Embrace On =  "..tostring(PetHealTarget.name)..",  Hp Percentage at =  "..tostring(PetHealTarget.hp.percent))
+			end
+			if ( pet ~= nil and pet ~= 0) then
+				if (acPetHeal.isready) then
+					acPetHeal:Cast()
+				end
+			end
+		end
+end
+
+
+function GetBestSummonHealTarget()
+	local el = EntityList("lowesthealth,friendly,chartype=4,myparty,alive,maxdistance="..tostring(35))
+	if ( el ) then
+		local i,e = next(el)
+		if (i~=nil and e~=nil) then
+			return e
+		end
+	end
+	--ml_debug("GetBestHealTarget() failed with no entity found matching params")
+	return nil
 end
---
+++ Spells.lua
@@ -0,0 +1,27 @@
+function SwiftCastRez()
+	local DeadPlayer = 0
+	DeadPlayer = FindDead()
+	local Raise = ActionList("type=1")
+	local Rez = Raise[125]
+	local acRez = ActionList:Get(Rez.id,DeadPlayer.id,1)
+	if Player.job == (26 or 27 or 28) then
+		Rez = Raise[173]
+	end
+
+	local SwiftCast = ActionList("type=1")
+	local SCast = SwiftCast[150]
+	local acSCast = ActionList:Get(SCast.id,1)
+	if DeadPlayer ~= 0  then
+		if DoesPlayerHaveBuff(167) == true then
+			if (acRez.isready) then
+				acRez:Cast()
+			end
+		end
+		if DoesPlayerHaveBuff(167) == false then
+			if (acSCast.isready) then
+				acSCast:Cast()
+			end
+		end
+	end
+end
+
